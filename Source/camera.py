0115154de18fee394aac4f8ceb01b293e5dec886
FiresoftGH <most.chuti@gmail.com>
2022-10-26 18:04:50 +0700

    minor changes to camera.py

--- Source/camera.py
+++ Source/camera.py
@@ -35,20 +35,21 @@ while True:
 
     ret,frame=cap.read()
     flipped=cv2.flip(frame,flipCode=1)
-    frame1 = cv2.resize(flipped,(640,400))
+    frame1 = cv2.resize(flipped,(640,480))
     result = pose.process(frame1)
+
     # rgb_img=cv2.cvtColor(frame1,cv2.COLOR_BGR2BGR)
     # result=pose.process(rgb_img)
     #Print general details about observed body
-    print (result.pose_landmarks)
+    # print (result.pose_landmarks)
     
     #Uncomment below to see X,Y coordinate Details on single location in this case the Nose Location.
     
-    #try:
-    #    print('X Coords are', result.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * 640)
-    #    print('Y Coords are', result.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * 480)
-    #except:
-    #    pass
+    try:
+       print('Shoulder: ', result.pose_landmarks.landmark[11].y * 640)
+       print('Right Hand thing: ', result.pose_landmarks.landmark[19].y * 480)
+    except: 
+       pass
     
     #Draw the framework of body onto the processed image and then show it in the preview window
     mpDraw.draw_landmarks(frame1,result.pose_landmarks,mp_pose.POSE_CONNECTIONS)
