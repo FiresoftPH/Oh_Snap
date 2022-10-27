5dc7fb0cba797ca8adb9fd034dc6c6f4889444ff
FiresoftGH <most.chuti@gmail.com>
2022-10-27 17:26:35 +0700

    Updated User_Interface_WIN.py

--- Source/User_Interface_WIN.py
+++ Source/User_Interface_WIN.py
@@ -5,9 +5,11 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry(f"{1280}x{800}")
+        # Main window Setup
+        self.geometry(f"1280x800")
         self.title("Oh Snap!")
         self.bind('<Escape>',lambda e: quit(e))
+
         # First Frame
         self.start_frame = customtkinter.CTkFrame(master = self,
                                                   width = 1280,
@@ -15,15 +17,15 @@ class App(customtkinter.CTk):
                                                   corner_radius = 0,
                                                   fg_color = "#BFD4FF")
         
-        self.dummy = Image.open('Oh_Snap/Pictures/sticker_3.png')
+        self.dummy = Image.open('Pictures\sticker_3.png')
         self.dummy_picture = ImageTk.PhotoImage(self.dummy)
 
         self.logo = customtkinter.CTkLabel(master = self.start_frame,
                                            image = self.dummy_picture)
 
-        self.start_btn = customtkinter.CTkButton(master = self.start_frame,
+        self.start_button = customtkinter.CTkButton(master = self.start_frame,
                                                  text = "Tap to start",
-                                                 text_font = ("Roboto Medium", 25, "underline"),
+                                                 text_font = ("Inter", 25, "underline"),
                                                  command = self.change_to_select_frame,
                                                  fg_color = "#BFD4FF",
                                                  hover = False,
@@ -31,28 +33,64 @@ class App(customtkinter.CTk):
         
         # Placing Elements in the first frame
         self.logo.grid(row = 0, column = 0, pady = 100, padx = 500)
-        self.start_btn.grid(row = 1, column = 0, pady = 100, padx = 10)
-        self.start_frame.pack(fill = "both", expand = 1) # Fullscreen frame?
 
-        # "Frame Selection" Frame
+        self.start_button.grid(row = 1, column = 0, pady = 100, padx = 10)
+
+        self.start_frame.pack(fill = "both", expand = 1)
+
+        # "Frame Selection" Elements
+
+        # Vertical frame widgets
+
         self.frame_selection = customtkinter.CTkFrame(master = self,
                                                       width = 1280,
-                                                      height = 1024,
+                                                      height = 800,
                                                       corner_radius = 0,
                                                       fg_color = "#BFD4FF")
         
-        self.vertical_frame = Image.open("Oh_Snap/Pictures/Frame_Vertical.png")
-        self.vertical_frame_picture = ImageTk.PhotoImage(self.vertical_frame)
+        self.vertical_frame = Image.open("Pictures\Frame_Vertical.png")
+        self.vertical_frame_resize = self.vertical_frame.resize((503, 791))
+   
+        self.vertical_frame_picture = ImageTk.PhotoImage(self.vertical_frame_resize)
 
         self.vertical_frame_button = customtkinter.CTkButton(master = self.frame_selection,
                                                              image = self.vertical_frame_picture,
                                                              hover = False,
+                                                             text = "",
+                                                             command = self.confirmation_pop_up,
                                                              fg_color = "#BFD4FF")
 
-        # Packing elements in the second frame
-        self.vertical_frame_button.grid(row = 0, column = 0, padx = 100, pady = 100)
-        self.frame_selection.pack(fill = "both", expand = 1)
+        self.vertical_frame_label = customtkinter.CTkLabel(master = self.frame_selection,
+                                                             text = "Vertical",
+                                                             text_font = ("Inter", 25, "underline"),
+                                                             text_color = "Black")
+        
+        # Horizontal frame widgets
 
+        self.horizontal_frame = Image.open("Pictures\Frame_Horizontal.png")
+        self.horizontal_frame_resize = self.horizontal_frame.resize((791, 503))
+        self.horizontal_frame_picture = ImageTk.PhotoImage(self.horizontal_frame_resize)
+
+        self.horizontal_frame_button = customtkinter.CTkButton(master = self.frame_selection,
+                                                               image = self.horizontal_frame_picture,
+                                                               hover = False,
+                                                               text = "",
+                                                               fg_color = "#BFD4FF",
+                                                               command = self.confirmation_pop_up)
+
+        self.horizontal_frame_label = customtkinter.CTkLabel(master = self.frame_selection,
+                                                             text = "Horizontal",
+                                                             text_font = ("Inter", 25, "underline"),
+                                                             text_color = "Black")
+
+        # Packing elements in the second frame
+
+        self.vertical_frame_label.grid(row = 0, column = 0, padx = 150, pady = 60, sticky = "new", rowspan = 10)
+        self.vertical_frame_button.grid(row = 1, column = 0, padx = 150, pady = 100, sticky = "nsew")
+        self.horizontal_frame_label.grid(row = 0, column = 1, padx = 150, pady = 140, sticky = "new", rowspan = 10)
+        self.horizontal_frame_button.grid(row = 1, column = 1, padx = 150, pady = 100, sticky = "nsew")
+
+        self.frame_selection.pack(fill = "both", expand = 1)
 
     def quit(self,e):
         self.destroy()
@@ -62,6 +100,41 @@ class App(customtkinter.CTk):
         self.start_frame.pack_forget()
         self.frame_selection.pack()
 
+    # Create a pop-up confirmation window
+    def confirmation_pop_up(self):
+
+        # Confirmation Pop-up window setup
+        confirmation_window = customtkinter.CTkToplevel(self)
+        confirmation_window.geometry("320x200")
+        confirmation_window.title("Frame")
+
+        # Confirmation Pop-up window frame
+        confirmation_frame = customtkinter.CTkFrame(master = confirmation_window,
+                                                     width = 320,
+                                                     height = 200,
+                                                     corner_radius = 0,
+                                                     fg_color = "#FFFFFF")
+        # Frame elements
+        confirmation_label = customtkinter.CTkLabel(master = confirmation_frame,
+                                                    text = "Are you sure?",
+                                                    text_font = ("Imprima", 15, "underline"),
+                                                    text_color = "black")
+
+        confirmation_button_yes = customtkinter.CTkButton(master = confirmation_frame,
+                                                          hover = False,
+                                                          text = "Yes",
+                                                          text_font = ("Imprima", 15),
+                                                          text_color = "white",
+                                                          width = 60,
+                                                          height = 30,
+                                                          corner_radius = 8)
+        # Packing elements in the pop-up frame
+        confirmation_frame.pack(fill = "both", expand = 1)
+        confirmation_label.grid(row = 0, column = 0, padx = 100, pady = 20)
+        # confirmation_label.pack(side = "top", padx = 80, pady = 20)
+        confirmation_button_yes.grid(row = 1, column = 0, padx = 0, pady = 0)
+
+        
     def change_to_taking_picture_frame():
         pass
 
