d6e43a576a9c6210f455abb6c67de1a98fa0eb75
Pymisme <64011558@kmitl.ac.th>
2022-11-14 17:26:09 +0700

    Add kyo filters and google drive test

---
+++ Source/GoogleDrive_Test/drive_test.py
@@ -0,0 +1,13 @@
+#1J6WzZyxidbrF7IsuK9SMsC0BXFEjr5SD
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)  
+
+upload_file_list = ['Kyo.jpg', 'grey.jpg']
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'parents': [{'id': '1J6WzZyxidbrF7IsuK9SMsC0BXFEjr5SD'}]})
+	# Read file and set it as the content of this instance.
+	gfile.SetContentFile(upload_file)
+	gfile.Upload() # Upload the file.
