39da7dd03e1eb9873b935894187248117be0338b
FiresoftGH <most.chuti@gmail.com>
2022-11-16 17:52:23 +0700

    Added some mediapipe functionality to camera.py

--- Source/test_pad.py
+++ Source/test_pad.py
@@ -1,25 +1,10 @@
-import customtkinter
+from time import time
 
-window = customtkinter.CTk()
+initial = time()
+while True:
+    current = time()
+    if current - initial >= 1:
+        print(current - initial)
+        initial = time()
 
-frame_1 = customtkinter.CTkFrame(master = window,
-                                 width = 100,
-                                 height = 100,
-                                 fg_color = "Green"
-                                 )
-
-frame_2 = customtkinter.CTkFrame(master = frame_1,
-                                 width = 50,
-                                 height = 50,
-                                 fg_color = "Blue")
-
-button_1 = customtkinter.CTkButton(master = frame_1)
-button_2 = customtkinter.CTkButton(master = frame_2,
-                                   text = "cringe")
-
-button_1.pack()
-button_2.pack()
-frame_1.pack()
-frame_2.pack()
-
-window.mainloop()
+    
